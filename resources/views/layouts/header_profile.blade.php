4d67b6339397f8be6bd8930addac0d5cd97ed54a
Pierre-Yves DERET <pierreyves.deret@gmail.com>
2018-06-19 21:11:03 +0200

    corrections user

---
+++ resources/views/layouts/header_profile.blade.php
@@ -0,0 +1,55 @@
+    <div class="youplay-banner banner-top youplay-banner-parallax small" style="max-height: 450px;">
+      <div class="image" style="background-image: url('{{ asset('images/game-journey-1400x460.jpg')}} ')">
+      </div>
+
+      <div class="youplay-user-navigation">
+        <div class="container">
+          <ul>
+            <li class="active"><a href="user-activity.html">Activity</a>
+            </li>
+            <li><a href="user-profile.html">Profile</a>
+            </li>
+            <li><a href="user-messages.html">Messages <span class="badge">6</span></a>
+            </li>
+            <li><a href="user-settings.html">Settings</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="info" style="max-height: 240px;">
+        <div>
+          <div class="container youplay-user">
+            <a href="assets/images/user-photo.jpg" class="angled-img image-popup">
+              <div class="img">
+                <img src="assets/images/user-avatar.jpg" alt="">
+              </div>
+              <i class="fa fa-search-plus icon"></i>
+            </a>
+            <div class="user-data">
+              <h2>John Doe</h2>
+              <div class="location"><i class="fa fa-map-marker"></i> Los Angeles</div>
+              <div class="activity">
+                <div>
+                  <div class="num">69</div>
+                  <div class="title">Posts</div>
+                </div>
+                <div>
+                  <div class="num">12</div>
+                  <div class="title">Games</div>
+                </div>
+                <div>
+                  <div class="num">689</div>
+                  <div class="title">Followers</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="container mt-20">
+            <a href="#!" class="btn btn-sm btn-default ml-0">Add Friend</a>
+            <a href="#!" class="btn btn-sm btn-default">Private Message</a>
+          </div>
+        </div>
+      </div>
+    </div>
