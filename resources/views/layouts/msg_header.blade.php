490af9af64171fc690f075af59a9c1b003f546d6
Pierre-Yves DERET <pierreyves.deret@gmail.com>
2018-06-25 14:44:33 +0200

    petit correctif

--- resources/views/layouts/msg_header.blade.php
+++ resources/views/layouts/msg_header.blade.php
@@ -2,13 +2,13 @@
     <div class="col-md-9">
         <ul class="pagination pagination-sm mt-0">
         <li 
-        <?php if (strpos(url()->current(), 'create') == false) {
+        <?php if (strpos(url()->current(), 'create') == false and strpos(url()->current(), 'show') == false) {
                   echo 'class="active"';
               }?>>
             <a href="{{ route('users.messages', Auth::user()->id ) }}">Boite de réception</a>
         </li>
         <li 
-        <?php if (strpos(url()->current(), 'create') !== false) {
+        <?php if (strpos(url()->current(), 'create') !== false or strpos(url()->current(), 'show') !== false) {
                   echo 'class="active"';
               }?>>
             <a href="{{ route('users.messages.create', Auth::user()->id ) }}">Écrire</a>
