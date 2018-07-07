288a1a1abbf9e51bc755fa4457afaa473ac1701d
oqomiz <jeremy.louis@cegetel.net>
2018-07-07 15:11:00 +0200

    Up tournois + add 404

--- resources/views/tournois.blade.php
+++ resources/views/tournois.blade.php
@@ -2,77 +2,108 @@
 
 <section class="content-wrap">
 
-    <div class="youplay-banner banner-top youplay-banner-parallax small">
-        <div class="image" style="background-image: url('/images/banner-news.jpg')">
-        </div>
+	<div class="youplay-banner banner-top youplay-banner-parallax small">
+		<div class="image" style="background-image: url('/images/banner-news.jpg')">
+		</div>
 
-        <div class="info">
-            <div>
-                <div class="container">
-                    <h1>Tout nos tournois</h1>
-                </div>
-            </div>
-        </div>
-    </div>
+		<div class="info">
+			<div>
+				<div class="container">
+					<h1>Tout nos tournois</h1>
+				</div>
+			</div>
+		</div>
+	</div>
 
-    <section class="youplay-banner youplay-banner-parallax small">
-        <div class="image" style="background-image: url('/images/banner-broken-age.jpg');">
-        </div>
+	<h2 class="container h1">Tournois Disponibles</h2>
+	<div class="container youplay-store store-grid">
+		<div class="col-md-9 isotope">
+			<ul class="pagination isotope-options">
+				<li data-filter="all" class="active"><span>All</span>
+				</li>
+				<li data-filter="lol"><span>League of Legends</span>
+				</li>
+				<li data-filter="fortnite"><span>Fortnite</span>
+				</li>
+			</ul>
 
-        <div class="info container align-center">
-            <div>
-                <h2>Prochain tournois</h2>
+			<div class="isotope-list row vertical-gutter">
 
-                <div class="countdown h2" data-end="2018/10/10"></div>
+				@foreach($tournois as $tournoi)
 
-                <br>
-                <br>
-                <a class="btn btn-lg" href="#!">S'inscrire</a>
-            </div>
-        </div>
-    </section>
+				<div class="item col-lg-4 col-md-6 col-xs-12" data-filters="lol">
+					<a href="{{ $tournoi->slug }}" class="angled-img">
+						<div class="img img-offset">
+							<img src="{{ Voyager::image( $tournoi->image ) }}" alt="">
+							<div class="badge show bg-default">En attente de joueurs</div>
+						</div>
+						<div class="bottom-info">
+							<h4>{{ $tournoi->libelle }}</h4>
+							<div class="row">
+								{{ substr($tournoi->description, 3, -4) }}
+							</div>
+						</div>
+					</a>
+				</div>
+				@endforeach
+
+			</div>
+		</div>
+	</div>
 
 
-    <div class="container youplay-store store-grid">
+	<section class="youplay-banner youplay-banner-parallax small">
+		<div class="image" style="background-image: url('/images/banner-broken-age.jpg');">
+		</div>
 
-        <!-- Games List -->
-        <div class="col-md-9 isotope">
-            <!-- Sort Categories -->
-            <ul class="pagination isotope-options">
-                <li data-filter="all" class="active"><span>All</span>
-                </li>
-                <li data-filter="lol"><span>League of Legends</span>
-                </li>
-                <li data-filter="fortnite"><span>Fortnite</span>
-                </li>
-            </ul>
-            <!-- /Sort Categories -->
+		<div class="info container align-center">
+			<div>
+				<h2>Prochain tournois</h2>
 
-            <div class="isotope-list row vertical-gutter">
+				<div class="countdown h2" data-end="2018/10/10"></div>
 
-                @foreach($tournois as $tournoi)
+				<br>
+				<br>
+				<a class="btn btn-lg" href="#!">S'inscrire</a>
+			</div>
+		</div>
+	</section>
 
-                <div class="item col-lg-4 col-md-6 col-xs-12" data-filters="lol">
-                    <a href="{{ $tournoi->slug }}" class="angled-img">
-                        <div class="img img-offset">
-                            <img src="{{ Voyager::image( $tournoi->image ) }}" alt="">
-                            <div class="badge show bg-default">1/3</div>
-                        </div>
-                        <div class="bottom-info">
-                            <h4>{{ $tournoi->libelle }}</h4>
-                            <div class="row">
-                                {{ substr($tournoi->description, 3, -4) }}
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                @endforeach
+	<h2 class="container h1">Tournois Passés</h2>
+	<div class="container youplay-store store-grid">
+		<div class="col-md-9 isotope">
+			<ul class="pagination isotope-options">
+				<li data-filter="all" class="active"><span>All</span>
+				</li>
+				<li data-filter="lol"><span>League of Legends</span>
+				</li>
+				<li data-filter="fortnite"><span>Fortnite</span>
+				</li>
+			</ul>
 
-            </div>
+			<div class="isotope-list row vertical-gutter">
 
-        </div>
+				@foreach($tournois as $tournoi)
 
-    </div>
+				<div class="item col-lg-4 col-md-6 col-xs-12" data-filters="lol">
+					<a href="{{ $tournoi->slug }}" class="angled-img">
+						<div class="img img-offset">
+							<img src="{{ Voyager::image( $tournoi->image ) }}" alt="">
+							<div class="badge show bg-default">Terminé</div>
+						</div>
+						<div class="bottom-info">
+							<h4>{{ $tournoi->libelle }}</h4>
+							<div class="row">
+								{{ substr($tournoi->description, 3, -4) }}
+							</div>
+						</div>
+					</a>
+				</div>
+				@endforeach
+
+			</div>
+		</div>
+	</div>
 
 
 </section>
