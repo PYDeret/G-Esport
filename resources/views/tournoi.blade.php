40d37f82489542f8db99027b597efe7169a3527f
NegreriePaul <paul.negrerie@gmail.com>
2018-07-07 17:17:37 +0200

    Work on details tournois

--- resources/views/tournoi.blade.php
+++ resources/views/tournoi.blade.php
@@ -1,8 +1,7 @@
 @include('layouts.header')
 
 
-
-@section('content')
+<!--
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
@@ -16,8 +15,53 @@
 
             </div>
         </div>
+    </div>-->
+
+<section class="content-wrap">
+
+    <div class="youplay-banner banner-top youplay-banner-parallax small">
+        <div class="image" style="background-image: url('/images/banner-battle.jpg')">
+        </div>
+
+        <div class="info">
+            <div>
+                <div class="container">
+                    <h1>{{ $tournoi->Libelle }}</h1>
+                </div>
+            </div>
+        </div>
     </div>
 
-@endsection
+    <div class="container youplay-news youplay-post">
+
+        <div class="col-md-9">
+            <article class="news-one">
+
+                <div class="description">
+                    <div class="angled-img pull-left video-popup">
+                        <div class="img">
+                            <img src="{{ Voyager::image( $tournoi->image ) }}" style="width:100%" alt="">
+                        </div>
+                    </div>
+                    <p>
+                    {!! $tournoi->description !!}
+                    <p>
+                        {{ $tournoi->DateDebut  }}  qui commence à {{ $tournoi->HeureDebut  }}
+                    </p>
+
+                    <p>
+                        {{ $tournoi->DateFin }}   qui fini à {{ $tournoi->HeureFin  }}
+                    </p>
+                </div>
+
+            </article>
+        </div>
+    </div>
+
+</section>
+
+
+
+
 
 @include('layouts.footer')
