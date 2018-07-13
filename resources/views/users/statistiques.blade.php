61a25617e2bc89be3521a1ecee4958186c7af238
NegreriePaul <paul.negrerie@gmail.com>
2018-07-13 18:40:59 +0200

    Done front statistiques

--- resources/views/users/statistiques.blade.php
+++ resources/views/users/statistiques.blade.php
@@ -8,38 +8,72 @@
         <div class="row">
 
             <div class="col-md-9">
+                <h3>Date d'inscription :</h3>
+                <table class="table table-bordered">
+                <tbody>
+                <tr>
+                    <td>
+                        <p>  {{ $stats->datedinscription->created_at }}</p>
+                    </td>
+                </tr>
+                </tbody>
+                </table>
 
-                <h4>Date d'inscription :</h4>
-
-                <p>  {{ $stats->datedinscription->created_at }}</p>
-
-
-                <h4><?php echo count($stats->tournoisplay);?> Tournois participés :</h4>
-
+                <h3><?php echo count($stats->tournoisplay);?> Tournois participés :</h3>
+                <table class="table table-bordered">
+                    <tbody>
                 @foreach ($stats->tournoisplay as $key => $value)
-
-                 <p> {{ $value->libelle }} - {!! $value->description !!} </p>
+                    <tr>
+                        <td id="libstats">
+                            <p> {{ $value->libelle }} </p>
+                        </td>
+                        <td id="descstats">
+                            <p> {!! $value->description !!} </p>
+                        </td>
+                   <td>
                     <span  class="date pull-left"><i class="fa fa-calendar"></i>{{ $value->DateDebut  }} - {{ $value->HeureDebut  }}</span>
-                    <span id="arrow" class="date pull-center"><i class="fa fa-arrow-right"></i></span>
+                       <span id="arrow" class="date pull-left"><i class="fa fa-arrow-right"></i></span>
                     <span class="date pull-right"><i class="fa fa-calendar"></i>{{ $value->DateFin }} à  {{ $value->HeureFin  }}</span>
+                   </td>
+                    </tr>
 
                 @endforeach
-
-                <h4><span id="counter"></span> Tournois remportés :</h4>
-
-                <?php $i=0;?>
-
-                @foreach ($stats->tournoisplay as $key => $value)
-
-                     @foreach ($stats->inteam as $keys => $values)
+                    </tbody>
+                </table>
 
 
-                         @if( $value->EquipeWin_id == $values->id )
+                <h3><span id="counter"></span> Tournois remportés :</h3>
+                <table class="table table-bordered">
+                    <tbody>
 
-                            <p> {{ $value->libelle }} - {!! $value->description !!} </p>
-                            <span  class="date pull-left"><i class="fa fa-calendar"></i>{{ $value->DateDebut  }} à {{ $value->HeureDebut  }}</span>
-                            <span id="arrow" class="date pull-center"><i class="fa fa-arrow-right"></i></span>
-                            <span class="date pull-right"><i class="fa fa-calendar"></i>{{ $value->DateFin }} à  {{ $value->HeureFin  }}</span>
+                    <?php $i=0;?>
+
+                    @foreach ($stats->tournoisplay as $key => $value)
+
+                        @foreach ($stats->inteam as $keys => $values)
+
+
+                            @if( $value->EquipeWin_id == $values->id )
+
+                                <tr>
+                        <td id="libstats">
+                            <p> {{ $value->libelle }} </p>
+                        </td>
+                                    <td id="descstats">
+                                        <p> {!! $value->description !!} </p>
+                                    </td>
+                        <td>
+                            <p>
+                                <span  class="date pull-left"><i class="fa fa-calendar"></i>{{ $value->DateDebut  }} à {{ $value->HeureDebut  }}</span>
+                                <span id="arrow" class="date pull-left"><i class="fa fa-arrow-right"></i></span>
+                                <span class="date pull-left"><i class="fa fa-calendar"></i>{{ $value->DateFin }} à {{ $value->HeureFin  }}</span>
+                            </p>
+                        </td>
+                    </tr>
+
+                    </tbody>
+                </table>
+
                             <?php $i++; ?>
                         @endif
 
@@ -49,26 +83,33 @@
 
                 <input type="hidden" id="count" value="<?= $i; ?>"/>
 
-                <h4>Mes equipes (<?php echo count($stats->inteam);?>):</h4>
+                <h3>Mes equipes (<?php echo count($stats->inteam);?>):</h3>
+                <table class="table table-bordered">
+                    <tbody>
 
-                @foreach ($stats->inteam as $key => $value)
+                    @foreach ($stats->inteam as $key => $value)
+                    <tr>
+                      <td id="libstats">
+                          <p> {{ $value->libelle }}</p>
+                          @if($value->userId == $user->id)
+                              <span class="badge badge-pill" style="background-color:#d91d1f; ">Chef d'équipe</span>
+                          @endif
+                      </td>
 
-                    <p> {{ $value->libelle }} - {!! $value->description !!}
+                        <td id="descstats">
+                            <p>{!! $value->description !!}</p>
 
-                        @if($value->userId == $user->id)
-
-                            <span class="badge badge-pill" style="background-color:#d91d1f; ">Chef d'équipe</span>
-
-                        @endif
-                    </p>
-                    <span  class="date pull-left"><i class="fa fa-calendar"></i>{{ $value->created_at  }} </span>
-                    <span id="arrow" class="date pull-center"><i class="fa fa-arrow-right"></i></span>
-                    <span class="date pull-right"><i class="fa fa-calendar"></i>{{ $value->created_at }} </span>
+                        </td>
+                        <td>
+                            <p><span  class="date pull-left"><i class="fa fa-calendar"></i>{{ $value->created_at  }} </span>
+                                <span id="arrow" class="date pull-left"><i class="fa fa-arrow-right"></i></span>
+                                <span class="date pull-right"><i class="fa fa-calendar"></i>{{ $value->created_at }} </span></p>
+                        </td>
+                    </tr>
 
                 @endforeach
-
-
-
+                    </tbody>
+                </table>
         </div>
     </div>
     </div>
@@ -84,3 +125,16 @@
 
     });
 </script>
+
+<style>
+
+    #libstats{
+        width: 21%;
+    }
+    #descstats{
+        width: 38%;
+
+    }
+
+
+</style>
