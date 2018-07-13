b4606e0cd4de835642bb9088bea9dca035764c41
Pierre-Yves DERET <pierreyves.deret@gmail.com>
2018-07-13 19:26:12 +0200

    bete modification

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -76,7 +76,7 @@
 			<div class="news-one">
 				<div class="row vertical-gutter">
 					<div class="col-md-4">
-						<a href="/news_in/{{ $unJeu->slug }}" class="angled-img">
+						<a href="/jeux_in/{{ $unJeu->slug }}" class="angled-img">
 							<div class="img">
 								<img src="{{ Voyager::image( $unJeu->img ) }}" alt="">
 							</div>
@@ -84,7 +84,7 @@
 					</div>
 					<div class="col-md-8">
 						<div class="clearfix">
-							<h3 class="h2 pull-left m-0"><a href="/news_in/{{ $unJeu->slug }}">{!! $unJeu->libelle !!}</a></h3>
+							<h3 class="h2 pull-left m-0"><a href="/jeux_in/{{ $unJeu->slug }}">{!! $unJeu->libelle !!}</a></h3>
 							<span class="date pull-right"><i class="fa fa-calendar"></i>{!! $unJeu->created_at !!}</span>
 						</div>
 						<div class="description">
@@ -92,7 +92,7 @@
 								{!! $unJeu->description !!}
 							</p>
 						</div>
-						<a href="/news_in/{{ $unJeu->slug }}" class="btn read-more pull-left">Voir plus</a>
+						<a href="/jeux_in/{{ $unJeu->slug }}" class="btn read-more pull-left">Voir plus</a>
 					</div>
 				</div>
 			</div>
