440a6463effca29ee84cb4b64ced917d2be5d907
NegreriePaul <paul.negrerie@gmail.com>
2018-07-15 14:46:29 +0200

    Fixed multiple bug

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -1,7 +1,7 @@
 @include('layouts.header')
 
 <div class="youplay-banner banner-top youplay-banner-parallax small">
-    <div class="image" style="background-image: url('/images/halo.jpg')">
+    <div class="image" style="background-image: url('/images/equipes_details.jpg')">
     </div>
 
     <div class="info">
@@ -30,10 +30,43 @@
                         if ($equipes_user->equipe_id == $equipe->id && $equipes_user->user_id == $user->id)
                         {
                         ?>
-                        <li>
-                            {{ $user->name }}
-                        </li>
+                            <a href="/users/check/{{$user->name }}" class="angled-img">
+                            <table class="table table-bordered">
+                                <tbody>
+                                <tr>
+                                    <th>
+                                        Avatar
+                                    </th>
+                                    <th>
+                                         Nom
+                                    </th>
+                                    <th>
+                                         Inscription
+                                    </th>
+                                </tr>
 
+
+                                <tr>
+                              <td style="width: 25%">
+                            <div class="img">
+                                <img src="{{ Voyager::image( $user->avatar ) }}" style="width:100%" alt="">
+                            </div>
+                            </td>
+
+                        <td style="width: 25%">
+
+                           <p> {{ $user->name }}</p>
+
+                        </td>
+                                    <td>
+                                        <p><span id="customdate" class="date pull-center"><i class="fa fa-calendar"></i>{{ $user->created_at  }} </span>  </p>
+                                    </td>
+
+                                </tr>
+
+                                </tbody>
+                            </table>
+                            </a>
                         <?php
                         }
                         ?>
